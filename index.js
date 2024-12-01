afe57dbb035f67f7189138ee658040ec0415f5d2
Aalok <aalok55thapa@gmail.com>
2024-12-02 00:15:34 +0545

    cart added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const productListHTML = document.querySelector(".product-list");
 const buttonElem = document.querySelector(".button");
 const plusMinusElem = document.querySelector(".plusMinus");
+const cartListHTML = document.querySelector(".cartList");
+
+let listProducts = [];
+let cart = [];
 
 const initApp = () => {
   fetch("data.json")
@@ -14,7 +18,7 @@ const initApp = () => {
 const addDataToHTML = () => {
   productListHTML.innerHTML = "";
   if (listProducts.length > 0) {
-    listProducts.forEach((product) => {
+    listProducts.forEach((product, key) => {
       let newProduct = document.createElement("div");
       newProduct.classList.add("item");
       newProduct.dataset.id = product.id;
@@ -27,7 +31,7 @@ const addDataToHTML = () => {
             <span class="itemCount">1</span>
             <span class="plus">+</span>
           </button>
-      <button class="button">
+      <button class="button" onClick ="addToCart(${key})">
       <img src="assets/images/icon-add-to-cart.svg">
       <span>Add to Cart</span></button>
       
@@ -39,15 +43,60 @@ const addDataToHTML = () => {
     });
   }
 };
-productListHTML.addEventListener("click", (event) => {
-  let positionClick = event.target;
-  if (
-    positionClick.classList.contains("button") ||
-    positionClick.parentElement.classList.contains("button")
-  ) {
-    let product_id = positionClick.parentElement.dataset.id;
-    alert(product_id);
+let addToCart = (product_id) => {
+  let positionThisProductInCart = cart.findIndex(
+    (value) => value.product_id == product_id
+  );
+  // if the value doesnt exist, it returns -1
+  if (cart.length <= 0) {
+    cart = [
+      {
+        product_id: product_id,
+        quantity: 1,
+      },
+    ];
+  } else if (positionThisProductInCart < 0) {
+    cart.push({
+      product_id: product_id,
+      quantity: 1,
+    });
+  } else {
+    cart[positionThisProductInCart].quantity =
+      cart[positionThisProductInCart].quantity + 1;
   }
-  console.log(positionClick.parentElement.name);
-});
+  console.log(cart);
+  addCartToHTML();
+};
+let addCartToHTML = () => {
+  cartListHTML.innerHTML = "";
+  let totalQuanity = 0;
+  if (cart.length > 0) {
+    cart.forEach((item) => {
+      let newCart = document.createElement("div");
+      newCart.classList.add("item");
+      newCart.dataset.id = item.product_id;
+
+      let positionProduct = listProducts.findIndex(
+        (value) => value.id == item.product_id
+      );
+      let info = listProducts[positionProduct];
+      cartListHTML.appendChild(newCart);
+      newCart.innerHTML = `
+         
+            <div class="details">
+              <div class="name">${info.name}</div>
+              <div class="numbers">
+                <span id="times">1x</span>&nbsp;
+                <span id="cost">&nbsp;@${info.price}</span>
+                <span id="total-cost">$${info.price}</span>
+              </div>
+            </div>
+            <div class="cross-sign">
+              <div>x</div>
+            </div>
+  `;
+    });
+    cartListHTML.appendChild(newCart);
+  }
+};
 initApp();
